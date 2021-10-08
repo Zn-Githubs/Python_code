18995b74f4790259a1b8a799a69e1f12984188d6
Zero_z_n <79430813+Zn-Githubs@users.noreply.github.com>
2021-10-08 14:03:50 +0800

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,1 +1,12 @@
-import threading
+# 需求：获取腾讯招聘前5页信息并保存到MongoDB中
+
+# 导入requests, pymongo
+import requests, pymongo
+# base_url
+base_url = 'https://careers.tencent.com/tencentcareer/api/post/Query'
+# headers_ua
+
+# params
+
+# 发起get请求获取响应
+# json数据转字典提取数据
